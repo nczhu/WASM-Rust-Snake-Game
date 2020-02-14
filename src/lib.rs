1739c8f7f1399073779c3ea8ed4df14d40225422
nczhu <nicole@parity.io>
2020-02-14 10:50:46 +0100

    Set up Structs game and vector to represent game mechanics

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,50 @@ extern {
 pub fn greet() {
     alert("Hello, rust-js-snake-game!");
 }
+
+#[wasm_bindgen]
+#[derive(Copy, Cloen)]
+pub struct Vector {
+    pub x: f64,
+    pub y: f64,
+}
+
+#[wasm_bindgen]
+impl Vector {
+    pub fn new(x: f64, y: f64) -> Vector {
+        Vector {x, y}
+    }
+}
+
+#[wasm_bindgen]
+pub struct Game {
+    pub width: i32,
+    pub height: i32,
+    pub speed: f64,
+    pub score: i32, 
+    pub direction: Vector,
+    pub food: Vector,
+    snake: Vec<Vector>,
+}
+
+impl Game {
+    #[wasm_bindgen(constructor)]
+    pub fn new(width: i32, height: i32, speed: f64, snake_length: i32, direction: Vector) -> Game {
+        let head_x = (f64::from(width) / 2_f64).round()- 0.5;
+        let head_y = (f64::from(height) / 2_f64).round() - 0.5;
+        let head = Vector::new(head_x, head_y);
+        let tailtip = head.subtract(&direction.scale_by(f64::from(snake_length)));
+        let snake = vec![tailtip, head];
+        // TODO: place 
+        let food = Vector::new(0.5, 0.5);
+
+        Game {
+            width, 
+            height, 
+            speed, 
+            snake, 
+            direction,
+            food
+        }
+    }
+}
